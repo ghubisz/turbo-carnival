f0edfc343110ca0c7f8dc9d8e12e0118eda6a8fa
ghubisz <gab.op.pl@op.pl>
2023-07-14 13:27:37 +0200

    ..

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import './features.css';
 
-const Features = () => {
+const featuresData = [
+  {
+    title: 'Improving end distrusts instantly',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
+  },
+  {
+    title: 'Become the tended active',
+    text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.',
+  },
+  {
+    title: 'Message or am nothing',
+    text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.',
+  },
+  {
+    title: 'Really boy law county',
+    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush..',
+  },
+];
+
+const Features = ({ title, text }) => {
   return (
-    <div>
-        Features
+    <div className="gpt3__features-container__feature">
+        <div className="gpt3__features-container__feature-title">
+          <div/>
+          <h1>{title}</h1>
+        </div>
+        <div className="gpt3__features-container_feature-text">
+          {text}
+        </div>
     </div>
   )
 }
 
-export default Features
+export default Features;
