03e3dfef0ed44b4e3fefb15eca6708a4248c0851
ghubisz <gab.op.pl@op.pl>
2023-07-18 21:46:06 +0200

    ..

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({imgUrl, date, text}) => {
   return (
     <div className="gpt3__blog-container_article">
         <div className="gpt3__blog-container_article-image">
